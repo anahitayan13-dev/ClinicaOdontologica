c9cee5b622cab3fd38b54874fe7d468fdbe606f4
Anahi Tayan <anahitayan13@gmail.com>
2026-09-23 15:01:48 -0500

    Creacion tabla debil

---
+++ ClinicaOdontologicaModelos/Detallecita.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClinicaOdontologicaModelos
+{
+    [Table("detallescita")]
+    public class DetalleCita
+    {
+        [Key]
+        [Column("id_detalle_cita", TypeName = "Serial")]
+        public int IdDetalleCita { get; set; }
+
+        [Column("costo_aplicado", TypeName = "numeric(10,2)")]
+        public decimal costoAplicado { get; set; }
+
+        [Column("observaciones")]
+        [MaxLength(200)]
+        public string observaciones { get; set; }
+
+        [ForeignKey("cita")]
+        [Column("id_cita")]
+        public int IdCita { get; set; }
+        public Cita? cita { get; set; }
+
+        [ForeignKey("tratamiento")]
+        [Column("id_tratamiento")]
+        public int IdTratamiento { get; set; }
+        public Tratamiento? tratamiento { get; set; }
+    }
+}
---
+++ ClinicaOdontologicaModelos/HistorialMedico.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClinicaOdontologicaModelos
+{
+   
+    [Table("historialesmedicos")]
+    public class HistorialMedico
+    {
+        [Key]
+        [Column("id_historial", TypeName = "Serial")]
+        public int IdHistorialMedico { get; set; }
+
+        [Column("alergias")]
+        [MaxLength(200)]
+        public string alergia { get; set; } = "Ninguna";
+
+        [Column("enfermedades_previas")]
+        [MaxLength(200)]
+        public string enfermedadesPrevias { get; set; } = "Ninguna";
+
+        [Column("tipo_sangre")]
+        [MaxLength(5)]
+        public string tipoSangre { get; set; }
+
+        [ForeignKey("paciente")]
+        [Column("id_paciente")]
+        public int IdPaciente { get; set; }
+        public Paciente? paciente { get; set; }
+    }
+    }
+}
